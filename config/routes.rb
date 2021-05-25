05835a89dd650385fd8916c0634b518bd0fae537
michael gastal <michaelgastal@gmail.com>
2021-05-25 15:43:52 +0200

    TaskManager is ok woth all the functions and code is refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # READ
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
-
   # CREATE
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # READ
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
   # UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
   # DELETE
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
